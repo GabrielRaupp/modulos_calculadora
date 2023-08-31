2149351175e75782bea961ae25079df919497c36
Gabriel Raupp <gabiraupppimentel@gmail.com>
2023-08-31 08:30:36 -0300

    Gabriel_Raupp

--- calculadora.js
+++ calculadora.js
@@ -2,9 +2,12 @@ var somar = require("./modulos/somar");
 var media = require("./modulos/media");
 var multiplicação = require("./modulos/multiplicação");
 var divisao = require("./modulos/divisão");
+var menos  = require("./modulos/menos");
 
 
 console.log(somar(40,25));
 console.log(media(40,25));
 console.log(divisao(40,25));
 console.log(multiplicação(40,25));
+console.log(menos(40,25));
+
---
+++ modulos/menos.js
@@ -0,0 +1,4 @@
+var menos = function (x,y) {
+    return x - y;
+  }; 
+  module.exports = menos;
